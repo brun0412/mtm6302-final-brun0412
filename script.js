1aa6d0d1148c8a2d54fad046a9a0481707ac06f4
Lauren Brunet <brun0412@algonquinlive.com>
2026-04-17 13:28:43 -0400

    Creates HTML/CSS, adds variables for most needed HTML elements

---
+++ script.js
@@ -0,0 +1,10 @@
+const $menus=document.getElementById('menus');
+const $recipes=document.getElementById('recipes');
+const $cookbook=document.getElementById('cookbook');
+const $expand=document.getElementById('expand');
+const $modal=document.getElementById('modal');
+const $save=document.getElementById('save');
+const $close=document.getElementById('close');
+const $recipe=document.getElementById('recipe');
+
+//https://www.themealdb.com/api/json/v1/1/search.php?s=soup
